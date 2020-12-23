30ec4328a176093f2a47c1703566dae6d421551a
Jonathan Slauter <jonathanslauter@gmail.com>
2020-12-23 11:36:43 -0800

    find vowels

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -5,7 +5,16 @@ e.g vowelsCounter('anehizxcv') // will return 3
 
 
 function vowelsCounter(text) {
-    // Code goes here
+    // Search text with Regex and store all matching instances
+    let matchingInstances = text.match(/[aeiou]/gi);
+
+    // Check if matching instances exist then calculate length
+    if (matchingInstances) {
+        // Return number of vowels
+        return matchingInstances.length
+    } else {
+        return 0
+    }
 }
 
 
