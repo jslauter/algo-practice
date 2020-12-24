e1a58ca08375f7b26f1511fe248f1f09269e0305
Jonathan Slauter <jonathanslauter@gmail.com>
2020-12-23 23:38:04 -0800

    reverse string

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -1,12 +1,17 @@
 /* CHALLENGE
-Given a string of text, write an algorithm that returns the text received in a reversed format. 
+Given a string of text, write an algorithm that returns the text received in a reversed format.
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
 
 function reverseString(text) {
-    // Code goes here
+  let result = ""
+
+  for(lett of text){
+    result += text[i]
+  }
+  return result
 }
 
 
