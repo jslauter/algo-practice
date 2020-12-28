b8f08e790f238e840ef7abb455cbc0ad61412f39
Jonathan Slauter <jonathanslauter@gmail.com>
2020-12-27 18:05:33 -0800

    longest word

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -1,12 +1,19 @@
 /* CHALLENGE
-Given a string of text, write an algorithm that returns the text received in a reversed format. 
+Given a string of text, write an algorithm that returns the text received in a reversed format.
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
 
 function longestWord(text) {
-    // Code goes here
+    var result = text.split(' ').reduce((maxLengthWord, currentWord) => {
+        if (currentWord.length > maxLengthWord.length) {
+            return currentWord
+        } else {
+            return maxLengthWord
+        }
+    }, "")
+    return result
 }
 
 
