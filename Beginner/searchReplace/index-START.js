ea2be860d3196601b3030f2e3ee4ee1615e1e5e7
Jonathan Slauter <jonathanslauter@gmail.com>
2020-12-28 18:58:12 -0800

    replace word

---
+++ Beginner/searchReplace/index-START.js
@@ -0,0 +1,6 @@
+function searchReplace(str, word, newWord){
+    if ( word[0] === word[0].toUpperCase() ) {
+        newWord = newWord[0].toUpperCase() + newWord.slice(1)
+   }
+   return str.replace(word, newWord)
+}
