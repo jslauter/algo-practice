95df2b6fb0f3286cf22708746cf941b9c3da7feb
Jonathan Slauter <jonathanslauter@gmail.com>
2020-12-31 20:11:17 -0800

    chunking

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -1,11 +1,14 @@
-/* 
+/*
  Given two or more arrays, write a function that combines
- their elements into one array without any repetition. 
+ their elements into one array without any repetition.
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
 function chunkArray(array, size) {
-    // Code goes here
+   if(array.length <= size){
+       return [array]
+   }
+   return [array.slice(0,size), ...chunkArray(array.slice(size), size)]
 }
 
 
