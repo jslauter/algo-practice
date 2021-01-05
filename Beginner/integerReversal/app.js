3e0da48b0727d28802be6d429034fa9c1ce01116
Jonathan Slauter <jonathanslauter@gmail.com>
2021-01-04 19:03:46 -0800

    integer reversal

---
+++ Beginner/integerReversal/app.js
@@ -0,0 +1,11 @@
+// Reverses a string
+function reverseString(text) {
+    return [...text].reduce((acc, char) => char + acc, '')
+}
+
+// Reverses an integer
+function reverseInteger(num) {
+    let reversedNumber = parseInt(reverseString(num.toString()))
+
+    return (reversedNumber * Math.sign(num))
+}
